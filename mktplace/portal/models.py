92f93d3d5c8303ee2449d318d8210ac0f8cd7941
raulbolsantos <raulbolsantos@gmail.com>
2017-09-19 00:04:08 -0300

    Finalizado criação de Models e configuração de áreas administrativas

--- mktplace/portal/models.py
+++ mktplace/portal/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -8,3 +9,30 @@ class Category(models.Model):
                                related_name='cat_child')
     order = models.IntegerField(null=True, blank=True)
     hidden = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    user = models.OneToOneField(User)
+    categories = models.ManyToManyField(Category,
+                                        blank=True, related_name="categories")
+    quantity = models.IntegerField(default=1)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    short_description = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    STATUS_CHOICES = (
+        ('Active', 'Active'),
+        ('Inactive', 'Inactive'),
+    )
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES,
+                              default='Inactive')
+
+    def __str__(self):
+        return self.name
